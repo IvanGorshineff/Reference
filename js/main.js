fdbf861b4f6d378de69fd6430f6c811bb408bd41
Ivan gorshinev <IvanQAEngineer@yandex.ru>
2023-07-25 21:23:47 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/// ---cлайдер---///
+
 $(function(){
     $('.bike-slider, .slider__items').slick({
         arrows:false,
@@ -8,3 +10,33 @@ $(function(){
     });
     
 });
+
+///---модальное окно---////
+
+
+const button = document.querySelector('[data-modal-logo]');
+const modal = document.querySelector('[data-modal]');
+const btnClose = document.querySelector('[data-modal-close]');
+
+button.addEventListener('click', function () {
+  modal.classList.remove('hidden');
+})
+
+btnClose.addEventListener('click', function () {
+  modal.classList.add('hidden');
+})
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $('.parallax').parallax();
+});
+
+
+instance.destroy();
